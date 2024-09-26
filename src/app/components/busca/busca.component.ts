b1d3b0a0bb34f7e28b9f7609816318966984b13d
Vanessa Furtado Nunes <vanessafurtadonunes@gmail.com>
2024-09-26 15:13:49 -0300

    Implementado componente de busca

---
+++ src/app/components/busca/busca.component.ts
@@ -0,0 +1,33 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-busca',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './busca.component.html',
+  styleUrl: './busca.component.scss'
+})
+export class BuscaComponent {
+
+  @Output() pesquisaFeita: EventEmitter<string>;
+  @Output() limpezaFiltro: EventEmitter<void>;
+
+  public buscaRealizada: boolean;
+
+  constructor(){
+    this.pesquisaFeita = new EventEmitter();
+    this.limpezaFiltro = new EventEmitter();
+
+    this.buscaRealizada = false;
+  }
+
+  public onBuscar(texto: string): void{
+    this.buscaRealizada = true;
+    this.pesquisaFeita.emit(texto);
+  }
+
+  public onLimpar(): void{
+    this.limpezaFiltro.emit();
+  }
+}
